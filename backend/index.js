ef8a7debea9d44a8a5bbbaafdaeff1a3f905634f
Shaktiranjan Sethy <sethyshaktiranjan@gmail.com>
2026-03-07 09:30:24 +0530

    home route added

--- backend/index.js
+++ backend/index.js
@@ -42,6 +42,9 @@ app.use(cors({
 }));
 app.use(express.json())
 app.use(cookieParser())
+app.get("/", (req, res) => {
+  res.send("Food Delivery API is running 🚀");
+});
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/shop", shopRouter)
